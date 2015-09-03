fff858e6cf264324fe20b74ec3fde40e8e58b7fd
e-karma <djbradyla@gmail.com>
2015-09-03 10:20:11 -0400

    ember-cli-bootstrap-sass removed

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -18,7 +18,6 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {});
-  app.import('bower_components/ember/ember-template-compiler.js');
 
   app.import('bower_components/bootstrap/dist/js/bootstrap.js');
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
